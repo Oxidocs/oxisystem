efd774af8c7ac400b103f4edd52053ac4ecc86c5
fabian_v <fvargassanchez34@gmail.com>
2016-08-07 06:39:19 -0400

    Modelo Contenido V 1.3

--- admin/models/contenido.php
+++ admin/models/contenido.php
@@ -31,61 +31,66 @@ class Contenido {
 	
 	public static function getContent($id_seccion) {
 
-		$model = new Crud();
-		$model->select = "`contenido`.`ID`,
-						`contenido`.`SECCIONES_ID`,
-						`contenido`.`ESTADOS_ID`,
-						`contenido`.`TITULO`,
-						`contenido`.`DESCRIPCION`";
-		$model->from = "`contenido`";
-		$model->condition = "`contenido`.`SECCIONES_ID` = $id_seccion AND `contenido`.`ESTADOS_ID` = 1";
-		$model->Read();
-		$response = $model->rows;
-
 		$content_list = array();
-		
-		foreach ($response as $content) {
 
-			$id = $content['ID']; //id obtenido de contenido para realizar las demás consultas
-
+		if ($id_seccion > 0) {
+			
 			$model = new Crud();
-			$model->select = "`path_imagenes`.`ID`,
-							`path_imagenes`.`ESTADOS_ID`,
-							`path_imagenes`.`PATH`,
-							`path_imagenes`.`TITULO`,
-							`path_imagenes`.`DESCRIPCION`";
-			$model->from = "`contenido`
-							INNER JOIN `contenido_path` ON `contenido`.`ID` = `contenido_path`.`CON_ID`
-							INNER JOIN `path_imagenes` ON `contenido_path`.`PAT_ID` = `path_imagenes`.`ID`";
-			$model->condition = "`contenido`.`ID` = $id AND `path_imagenes`.`ESTADOS_ID` = 1";
+			$model->select = "`contenido`.`ID`,
+							`contenido`.`SECCIONES_ID`,
+							`contenido`.`ESTADOS_ID`,
+							`contenido`.`TITULO`,
+							`contenido`.`DESCRIPCION`";
+			$model->from = "`contenido`";
+			$model->condition = "`contenido`.`SECCIONES_ID` = $id_seccion AND `contenido`.`ESTADOS_ID` = 1";
 			$model->Read();
-			$imgs = $model->rows;
+			$response = $model->rows;
+			
+			foreach ($response as $content) {
 
-			$model = new Crud();
-			$model->select = "`redes_sociales`.`ID`,
-							`redes_sociales`.`TIPO_REDES_ID`,
-							`redes_sociales`.`ESTADOS_ID`,
-							`redes_sociales`.`CON_ID`,
-							`redes_sociales`.`LINK`";
-			$model->from = "`redes_sociales`";
-			$model->condition = "`redes_sociales`.`CON_ID` = $id AND `redes_sociales`.`ESTADOS_ID` = 1";
-			$model->Read();
-			$social_media = $model->rows;
+				$id = $content['ID']; //id obtenido de contenido para realizar las demás consultas
 
-			$model = new Crud();
-			$model->select = "`links`.`ID`,
-							`links`.`CONTENIDOS_ID`,
-							`links`.`ESTADOS_ID`,
-							`links`.`URL`";
-			$model->from = "`links`";
-			$model->condition = "`links`.`CONTENIDOS_ID` = $id AND `links`.`ESTADOS_ID` = 1";
-			$model->Read();
-			$enlaces = $model->rows;
+				$model = new Crud();
+				$model->select = "`path_imagenes`.`ID`,
+								`path_imagenes`.`ESTADOS_ID`,
+								`path_imagenes`.`PATH`,
+								`path_imagenes`.`TITULO`,
+								`path_imagenes`.`DESCRIPCION`";
+				$model->from = "`contenido`
+								INNER JOIN `contenido_path` ON `contenido`.`ID` = `contenido_path`.`CON_ID`
+								INNER JOIN `path_imagenes` ON `contenido_path`.`PAT_ID` = `path_imagenes`.`ID`";
+				$model->condition = "`contenido`.`ID` = $id AND `path_imagenes`.`ESTADOS_ID` = 1";
+				$model->Read();
+				$imgs = $model->rows;
 
-			$contenido = new Contenido($id, $content['SECCIONES_ID'],$content['ESTADOS_ID'],$content['TITULO'],$content['DESCRIPCION'],$imgs,$social_media, $enlaces);
+				$model = new Crud();
+				$model->select = "`redes_sociales`.`ID`,
+								`redes_sociales`.`TIPO_REDES_ID`,
+								`redes_sociales`.`ESTADOS_ID`,
+								`redes_sociales`.`CON_ID`,
+								`redes_sociales`.`LINK`";
+				$model->from = "`redes_sociales`";
+				$model->condition = "`redes_sociales`.`CON_ID` = $id AND `redes_sociales`.`ESTADOS_ID` = 1";
+				$model->Read();
+				$social_media = $model->rows;
 
-			array_push($content_list, $contenido);
+				$model = new Crud();
+				$model->select = "`links`.`ID`,
+								`links`.`CONTENIDOS_ID`,
+								`links`.`ESTADOS_ID`,
+								`links`.`URL`";
+				$model->from = "`links`";
+				$model->condition = "`links`.`CONTENIDOS_ID` = $id AND `links`.`ESTADOS_ID` = 1";
+				$model->Read();
+				$enlaces = $model->rows;
+
+				$contenido = new Contenido($id, $content['SECCIONES_ID'],$content['ESTADOS_ID'],$content['TITULO'],$content['DESCRIPCION'],$imgs,$social_media, $enlaces);
+
+				array_push($content_list, $contenido);
+			}
 		}
+
+		
 
 		return $content_list;
 	}
