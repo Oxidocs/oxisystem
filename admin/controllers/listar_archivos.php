053dc348b3e07d2f28a0f0a7afb0f48ab875bbb1
muhlenbrock <racm@live.cl>
2016-08-11 23:27:45 -0300

    controller listar_archivo.php
    agregue funcion

--- admin/controllers/listar_archivos.php
+++ admin/controllers/listar_archivos.php
@@ -2,6 +2,8 @@
 
 if (isset($_POST['dir'])) {
 	getFilesName($_POST['dir']);
+}elseif (isset($_POST['dir_galeria'])) {
+	getFilesGaleria($_POST['dir_galeria']);
 }
 
 function getFilesName($url){
@@ -10,6 +12,17 @@ function getFilesName($url){
     foreach ($files as $file) {
     	if ($file!="."&&$file!="..") {
     		array_push($files_array, "<option value='$file'>".$file."</option>");
+    	}
+    }
+    echo json_encode($files_array);
+}
+
+function getFilesGaleria($url){
+	$files_array = array();
+    $files = scandir($url);
+    foreach ($files as $file) {
+    	if ($file!="."&&$file!="..") {
+    		array_push($files_array, $file);
     	}
     }
     echo json_encode($files_array);
