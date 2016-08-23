321a3ffd1d611daba45d0f62c0c1ec676af6963a
MPL80 <mauricio.leonheart@gmail.com>
2016-08-23 20:40:42 -0300

    Creación de carpetas
    Se creo la carpeta include, y se agregaron header, footer, nav y slider
    con php

---
+++ include/footer.php
@@ -0,0 +1,19 @@
+<!-- Footer -->
+<footer class="logosydatos">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6">
+        <img src="img/logoweb.png" alt="" class="img-responsive pull-left">
+      </div>
+      <div class="col-lg-6">
+        <address class="pull-right">
+          <strong>Escuela de Periodismo Universidad de La Serena</strong><br>
+          Campus Andrés Bello, Avenida Raúl Bitrán Nachary S/N<br>
+          La Serena, Chile<br>
+          <abbr title="Télefono"></abbr> +56 (51) 2204352<br>
+          <a href="mailto:#">eperiodismo@userena.cl</a>
+        </address>
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ include/nav.php
@@ -0,0 +1,71 @@
+<!-- Navegacion -->
+<nav class="navbar navbar-inverse" role="navigation">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <!--                <a class="navbar-brand" href="index.html">Start Bootstrap</a>-->
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active">
+          <a href="index.html">Home</a>
+        </li>
+        <li class="dropdown">
+          <a href="escuela.html" class="dropdown-toggle" data-toggle="dropdown">Escuela <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li>
+              <a href="escuela.html">Misión</a>
+            </li>
+            <li>
+              <a href="escuela.html">Visión</a>
+            </li>
+            <li>
+              <a href="escuela.html">Normativas</a>
+            </li>
+            <li>
+              <a href="escuela.html">Pautas de investigación y exámenes</a>
+            </li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Carrera <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li>
+              <a href="perfil.html">Perfil</a>
+            </li>
+            <li>
+              <a href="#">Admisión</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="docentes.html">Cuerpo Docente</a>
+        </li>
+        <li>
+          <a href="noticias.html">Noticias</a>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Publicaciones <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li>
+              <a href="publicaciones.html">Artículos de Académicos</a>
+            </li>
+            <li>
+              <a href="publicaciones.html">Seminarios de Investigación</a>
+            </li>
+          </ul>
+        </li>
+
+      </ul>
+    </div>
+    <!-- /.navbar-collapse -->
+  </div>
+  <!-- /.container -->
+</nav>
