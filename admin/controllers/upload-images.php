96a1e0e9fd7f8cc9f7f964722510c23b7eb77b99
muhlenbrock <racm@live.cl>
2016-09-04 23:19:31 -0300

    upload dropzone

--- admin/controllers/upload-images.php
+++ admin/controllers/upload-images.php
@@ -19,10 +19,14 @@ function uploadImages($url){
 				echo json_encode(array("res" => true));
 			}
 		} else {
+
 			$tempFile = $_FILES['file']['tmp_name'];
 		    $targetPath = dirname( __FILE__ ) . $ds. $url . $ds; 
-		    $targetFile =  $targetPath. $_FILES['file']['name']; 
+		    $nombrelimpio = limpiar_string($_FILES['file']['name']);
+		    $targetFile =  $targetPath. $nombrelimpio; 
+		   
 		    move_uploaded_file($tempFile,$targetFile);
+		    
 		}
 	 
 	} else {
@@ -40,6 +44,7 @@ function uploadImages($url){
 			$result  = array();
 	 
 		    $files = scandir($url);
+		   // print_r($files);
 		    if ( false!==$files ) {
 		        foreach ( $files as $file ) {
 		            if ( '.'!=$file && '..'!=$file) {
@@ -57,5 +62,24 @@ function uploadImages($url){
 	}
 }
 
+function limpiar_string($s) {
+		$s = mb_convert_encoding($s, 'UTF-8','');
+		$s = preg_replace("/á|à|â|ã|ª/","a",$s);
+		$s = preg_replace("/Á|À|Â|Ã/","A",$s);
+		$s = preg_replace("/é|è|ê/","e",$s);
+		$s = preg_replace("/É|È|Ê/","E",$s);
+		$s = preg_replace("/í|ì|î/","i",$s);
+		$s = preg_replace("/Í|Ì|Î/","I",$s);
+		$s = preg_replace("/ó|ò|ô|õ|º/","o",$s);
+		$s = preg_replace("/Ó|Ò|Ô|Õ/","O",$s);
+		$s = preg_replace("/ú|ù|û/","u",$s);
+		$s = preg_replace("/Ú|Ù|Û/","U",$s);
+		$s = str_replace(" ","_",$s);
+		$s = str_replace("ñ","n",$s);
+		$s = str_replace("Ñ","N",$s);
+		
+		$s = preg_replace('/[^a-zA-Z0-9_.-]/', '', $s);
+		return $s;
+	}
 
 ?>
