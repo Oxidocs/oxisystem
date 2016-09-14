e8807bc0899a48346e5e1c1a12b6c61d261da2e8
MPL80 <mauricio.leonheart@gmail.com>
2016-09-14 00:30:43 -0300

    Actualización 666
    se agregaron los metas y el favicon, y además se agrega una archivo
    gitignore para que no pesque los archivos de mac ni los map de sass.

--- escuela.php
+++ escuela.php
@@ -6,8 +6,9 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="">
-	<meta name="author" content="">
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="sitio, institucional, userena, uls, periodismo" />
+	<meta name="description" content="Sitio de la carrera de Periodismo de la Universidad de La Serena" />
 
 	<title>ULS | Escuela de Periodismo</title>
 
