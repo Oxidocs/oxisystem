91ef085092859f9ed3694f5ca9fbb3523492028a
MPL80 <mauricio.leonheart@gmail.com>
2016-09-14 00:24:04 -0300

    Se agregaron metas y favicon
    eso

--- galerias.php
+++ galerias.php
@@ -6,8 +6,9 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="">
-	<meta name="author" content="">
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="sitio, institucional, userena, uls, periodismo" />
+	<meta name="description" content="Sitio de la carrera de Periodismo de la Universidad de La Serena" />
 
 	<title>ULS | Escuela de Periodismo</title>
 
@@ -47,7 +48,7 @@
 
   <section class="seccion-noticias">
     <div class="container">
-      <?php 
+      <?php
         include 'include/paginador_galerias.php';
       ?>
 
