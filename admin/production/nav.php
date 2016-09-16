fa96cfe2a631c68934ec56dd54e34eb6d9702bf6
fabian_v <fvargassanchez34@gmail.com>
2016-09-16 02:03:48 -0300

    Se agrega nombre en navbar backoffice

--- admin/production/nav.php
+++ admin/production/nav.php
@@ -8,12 +8,15 @@
             <ul class="nav navbar-nav navbar-right">
                 <li class="">
                     <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                        John Doe
+                        <?php 
+                            $user = $_SESSION['user'];
+                            echo $user;
+                        ?>
                         <span class=" fa fa-angle-down"></span>
                     </a>
                     <ul class="dropdown-menu dropdown-usermenu pull-right">                        
                         <li><a href="javascript:;">Ayuda</a></li>
-                        <li><a href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                        <li><a href="logout.php"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
                     </ul>
                 </li>
             </ul>
